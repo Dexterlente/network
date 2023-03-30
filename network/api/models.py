e3f89935b4271713f21cc778739a4839599c8b1a
Dexterlente <dexter.lente18@gmail.com>
2023-03-30 22:02:52 +0800

    ww

--- network/api/models.py
+++ network/api/models.py
@@ -5,20 +5,36 @@ from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
 
 class User(AbstractUser):
-    pass
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    groups = models.ManyToManyField(
+        'auth.Group',
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        blank=True,
+        related_name='custom_user_set',
+        related_query_name='custom_user'
+    )
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     followers_count = models.PositiveIntegerField(default=0)
     following_count = models.PositiveIntegerField(default=0)
-    following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='followed_profiles')
     followers = models.ManyToManyField('self', symmetrical=False, related_name='following_profiles')
 
     def __str__(self):
         return self.user.username
+
 
 class Post(models.Model):
     content = models.CharField(max_length=280)
     created_date = models.DateTimeField(auto_now_add=True)
     poster = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="get_all_posts")
     likes = models.ManyToManyField(Profile, blank=True, related_name="get_all_liked_posts")
+
+    def __str__(self):
+        return self.poster
