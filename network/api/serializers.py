d24800f1323153d1eb1bb9bbdfccea131f62fdf1
Dexterlente <dexter.lente18@gmail.com>
2023-04-02 22:27:19 +0800

    WTF

--- network/api/serializers.py
+++ network/api/serializers.py
@@ -9,10 +9,12 @@ class ProfileSerializer(serializers.ModelSerializer):
     following = serializers.SerializerMethodField()
     currently_following = serializers.SerializerMethodField()
     follow_available = serializers.SerializerMethodField()
+    followers_list = serializers.SerializerMethodField()
+    following_list = serializers.SerializerMethodField()
 
     class Meta:
         model = Profile
-        fields = ('profile_id', 'profile_username', 'followers', 'following', 'currently_following', 'follow_available')
+        fields = ('profile_id', 'profile_username', 'followers', 'following', 'currently_following', 'follow_available', 'followers_list', 'following_list')
 
     def get_followers(self, obj):
         return obj.followers.count()
@@ -27,3 +29,13 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_follow_available(self, obj):
         user = self.context.get('request').user
         return (not user.is_anonymous) and obj.user != user
+
+    def get_followers_list(self, obj):
+        follower_profiles = obj.followers.all()
+        serializer = ProfileSerializer(follower_profiles, many=True, context=self.context)
+        return serializer.data
+
+    def get_following_list(self, obj):
+        following_profiles = obj.user.following.all()
+        serializer = ProfileSerializer(following_profiles, many=True, context=self.context)
+        return serializer.data
