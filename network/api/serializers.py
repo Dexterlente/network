9d8d29422ab489f657af5fd9389a1c0dfe60ac5e
Dexterlente <dexter.lente18@gmail.com>
2023-04-03 12:47:35 +0800

    serializer

--- network/api/serializers.py
+++ network/api/serializers.py
@@ -1,6 +1,51 @@
 from rest_framework.serializers import ModelSerializer
 from .models import User, Profile, Post
 from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+class UserSerializer(ModelSerializer):
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password_confirm', 'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        password_confirm = validated_data.pop('password_confirm')
+        if password != password_confirm:
+            raise serializers.ValidationError({'password': 'Passwords do not match'})
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    data['user'] = user
+                else:
+                    raise serializers.ValidationError('User account is disabled.')
+            else:
+                raise serializers.ValidationError('Unable to log in with provided credentials.')
+        else:
+            raise serializers.ValidationError('Must include "username" and "password".')
+
+        return data
+
+
+class LogoutSerializer(serializers.Serializer):
+    pass
 
 class ProfileSerializer(serializers.ModelSerializer):
     profile_id = serializers.ReadOnlyField(source='user.id')
