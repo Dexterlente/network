594fd9684107f05a27b384336d7cfdda87f72190
Dexterlente <dexter.lente18@gmail.com>
2023-04-03 13:21:25 +0800

    token permission

--- network/api/serializers.py
+++ network/api/serializers.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from .models import User, Profile, Post
 from rest_framework import serializers
 from django.contrib.auth import authenticate
+from django.core.exceptions import ValidationError
 
 class UserSerializer(ModelSerializer):
     password_confirm = serializers.CharField(write_only=True)
--- network/api/views.py
+++ network/api/views.py
@@ -1,6 +1,45 @@
 from django.shortcuts import render
 from .models import User, Profile, Post
+from rest_framework import permissions
+from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import generics, status, serializers
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+class LoginAPIView(ObtainAuthToken):
+    def post(self, request):
+        if request.user.is_authenticated:
+            return Response({'error': 'User is already authenticated.'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key, 'success': 'Logged in successfully'})
+        else:
+            return Response({'error': 'Invalid login credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+
+class LogoutView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, format=None):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(["POST"])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'message': 'Successfully registered', 'token': token.key})
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class profile_list(generics.ListAPIView):
     queryset = Profile.objects.all()
@@ -11,16 +50,16 @@ class profile_detail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     lookup_field = 'id'
-    # authentication_classes =  [TokenAuthentication] 
+    authentication_classes =  [TokenAuthentication] 
 
-    # def get_permissions(self):
-    #     permission_classes = []
-    #     if self.request.method == 'GET':
-    #         permission_classes = [AllowAny]
-    #     else:
-    #         permission_classes = [IsAuthenticated]
+    def get_permissions(self):
+        permission_classes = []
+        if self.request.method == 'GET':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
 
-    #     return [permission() for permission in permission_classes]
+        return [permission() for permission in permission_classes]
 
 class FollowerListAPIView(generics.ListAPIView):
     serializer_class = ProfileSerializer
