17db69797968ba3d1efe5c3bd0ea86ebed3c367b
Dexterlente <dexter.lente18@gmail.com>
2023-04-08 04:14:08 +0800

    settings

--- network/api/models.py
+++ network/api/models.py
@@ -9,19 +9,6 @@ from django.utils import timezone
 class User(AbstractUser):
     # following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
 
-    groups = models.ManyToManyField(
-        'auth.Group',
-        blank=True,
-        related_name='custom_user_set',
-        related_query_name='custom_user'
-    )
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        blank=True,
-        related_name='custom_user_set',
-        related_query_name='custom_user'
-    )
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
