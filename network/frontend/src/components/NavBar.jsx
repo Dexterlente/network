01ae50b133db45acfbbe07cf51e8f5252a708ba5
Dexterlente <dexter.lente18@gmail.com>
2023-04-15 01:07:30 +0800

    Sidebars

--- network/frontend/src/components/NavBar.jsx
+++ network/frontend/src/components/NavBar.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import Birdie from '../assets/Birdie.png'
+import { RiHome7Fill } from 'react-icons/ri'
+// import { RiHome7Line } from 'react-icons/ri'
+import { BsPerson } from 'react-icons/bs'
+// import { IoPerson } from 'react-icons/io'
+
 const NavBar = () => {
   return (
     <div>
       <div className='h-16 w-16'> 
         <img src={Birdie} alt='BirdieKo'  />
       </div>
-          <div className='text-'>
+        <div className='text-3xl'>
+          <div className='flex'>
+            <RiHome7Fill />
           Home
           </div>
-          <div>
+          <div className='flex'>
+            <BsPerson />
           Profile
           </div>
+        </div>
     </div>
   )
 }
