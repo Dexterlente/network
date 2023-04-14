8fd1da0c6d31c2856134457260e8415987c56d0c
Dexterlente <dexter.lente18@gmail.com>
2023-04-15 01:21:05 +0800

    navbar

--- network/frontend/src/components/NavBar.jsx
+++ network/frontend/src/components/NavBar.jsx
@@ -8,17 +8,20 @@ import { BsPerson } from 'react-icons/bs'
 const NavBar = () => {
   return (
     <div>
-      <div className='h-16 w-16'> 
+      <div className='h-16 w-16 hover:bg-gray-700 rounded-full'> 
         <img src={Birdie} alt='BirdieKo'  />
       </div>
         <div className='text-3xl'>
-          <div className='flex'>
-            <RiHome7Fill />
+          <div className='flex p-2'>
+          <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
+            <RiHome7Fill className='mt-1 mr-2'  />
           Home
+          </span>
           </div>
-          <div className='flex'>
-            <BsPerson />
-          Profile
+          <div className='flex p-2'>
+          <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
+            <BsPerson className='mt-1 mr-2' />
+                Profile </span>
           </div>
         </div>
     </div>
