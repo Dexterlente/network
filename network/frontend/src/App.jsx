e74c55b4d29261cce5553d93f8d9b4d633dc3b3a
Dexterlente <dexter.lente18@gmail.com>
2023-04-15 03:26:25 +0800

    navbar make

--- network/frontend/src/App.jsx
+++ network/frontend/src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import NavBar from './components/NavBar'
+import MainBody from './pages/MainBody'
+import { Outlet, Route, Routes } from 'react-router-dom'
+import { lazy, Suspense, useState } from "react";
+
+// const MainBody = lazy(() => import("./pages/MainBody"));
+const OtherPage = lazy(() => import("./pages/OtherPage"));
 
 function App() {
+  const NavSideBar = () => {
+    return (
+            <div className="grid grid-cols-3">
+              <div className="col-start-1 flex justify-end">
+                <NavBar className="self-end w-2/5" />
+              </div>
+            <Outlet className="col-span-2"/>
+            </div>
+          );
+      };
 
   return (
-      <>
-      <NavBar />
-      </>
+    <div className='overflow-hidden'>
+      
+          <Routes>
+            <Route path="/" element={<NavSideBar />}>
+              <Route path="/" element={<MainBody />} />
+              <Route path="/other" element={<OtherPage />} />
+            </Route>                  
+          </Routes>
+      </div>
+
   )
 }
 
--- network/frontend/src/components/NavBar.jsx
+++ network/frontend/src/components/NavBar.jsx
@@ -4,26 +4,42 @@ import { RiHome7Fill } from 'react-icons/ri'
 // import { RiHome7Line } from 'react-icons/ri'
 import { BsPerson } from 'react-icons/bs'
 // import { IoPerson } from 'react-icons/io'
+import { CgProfile } from 'react-icons/cg'
 
 const NavBar = () => {
+  
   return (
-    <div>
-      <div className='h-16 w-16 hover:bg-gray-700 rounded-full'> 
-        <img src={Birdie} alt='BirdieKo'  />
-      </div>
-        <div className='text-3xl'>
-          <div className='flex p-2'>
-          <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
-            <RiHome7Fill className='mt-1 mr-2'  />
-          Home
-          </span>
-          </div>
-          <div className='flex p-2'>
-          <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
-            <BsPerson className='mt-1 mr-2' />
-                Profile </span>
-          </div>
+    <div className='fixed top-0 mr-4'>
+      <div className='w-2/5 mt-3'>
+        <div className='h-16 w-16 hover:bg-gray-700 rounded-full'> 
+          <img src={Birdie} alt='BirdieKo'  />
         </div>
+          <div className='text-2xl'>
+            <div className='flex p-2'>
+            <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
+              <RiHome7Fill className='mt-1 mr-4'  />
+            Home
+            </span>
+            </div>
+            <div className='flex p-2'>
+            <span className='hover:bg-gray-200 rounded-full flex px-6 py-2'>
+              <BsPerson className='mt-1 mr-4' />
+                  Profile </span>
+            </div>
+            <div className='mt-3'>
+              <button className='bg-[#448EE4] rounded-full p-2 px-[78px] text-white font-bold text-lg'>
+                Tweet
+              </button>
+            </div>
+          </div>
+          <div className='fixed bottom-3 flex hover:bg-gray-300 rounded-full px-4'>
+          <CgProfile className='mt-1 mr-2 h-12 w-12'/>
+          <div>
+            <p>Dexter James Lente</p>
+              <p>@dexter</p>
+              </div>
+          </div>
+      </div>
     </div>
   )
 }
