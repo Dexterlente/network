14deec43b39669e1d98492d8bd5e096de90a0c7d
Dexterlente <dexter.lente18@gmail.com>
2023-04-15 14:06:11 +0800

    Change

--- network/frontend/src/components/RightBar.jsx
+++ network/frontend/src/components/RightBar.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const RightBar = () => {
   return (
-    <div className='fixed top-0 mt-2 ml-4'>
-        <div className='border-[1px] border-solid border-gray-300 rounded-lg p-6 w-3/5'>
+    <div className='fixed top-0 mt-2 ml-6'>
+        <div className='border-[1px] border-solid border-gray-300 rounded-lg text-center p-6 w-3/5'>
             <p className='font-bold text-2xl'>New to Twitter?</p>
             <p className='mb-4'>Sign up now to get your own personalized timeline!</p>
-            <button className='border-[1px] border-solid rounded-full px-20 py-2 font-bold hover:bg-gray-300'>Create account</button>
+            <button className='border-[1px] border-solid rounded-full px-10 py-2 font-bold hover:bg-gray-300'>Create account</button>
             <p className='text-sm mt-2'>By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.</p>
         </div>
         <div className='w-3/5 mt-2'>
--- network/frontend/src/pages/MainBody.jsx
+++ network/frontend/src/pages/MainBody.jsx
@@ -1,9 +1,105 @@
 import React from 'react'
-
+import pretty from '../assets/pretty.png'
+import { AiOutlineHeart } from 'react-icons/ai'
 const MainBody = () => {
   return (
-    <div className='border-x-2 border-gray-300'>
-      d
+    <div>
+      <div className='border-2 border-gray-300 hover:bg-gray-100'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mb-3 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
+      <div className='border-2 border-gray-300'>
+        <div className='flex mt-3 items-center'>
+            <div><img src={pretty} alt='DisplayPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' /></div>
+            <div className='ml-5 font-bold mt-2'> Dexter Lente</div>
+            <div className='ml-2 mt-2'>  @dexterjames</div>
+        </div>
+        <p className='w-4/5  mx-auto text-left'>lorem lorem sita buko ng papaya dalay dalay dusdos sisidlan and bunga</p>
+        <div className='ml-6 mt-3 flex'>
+          <p><AiOutlineHeart className='h-6 w-6 mr-1' /></p>
+          <p>100</p>
+          </div>
+      </div>
     </div>
   )
 }
