1bafa182e0ff79c919eec61ee1232011f894cf30
Dexterlente <dexter.lente18@gmail.com>
2023-04-15 22:13:14 +0800

    Login

--- network/frontend/src/pages/Login.jsx
+++ network/frontend/src/pages/Login.jsx
@@ -1,17 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
+import API_ENDPOINT from '../config.jsx'
+
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = { username: username, password: password };
+    console.log(Cookies.get('csrftoken'));
+    fetch(`${API_ENDPOINT}/api/login/`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': Cookies.get('csrftoken'),
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.success) {
+          // Successful login logic here
+          Cookies.set("csrftoken", data.csrftoken);
+          Cookies.set('token', data.token);
+          window.location.reload(true); // hard refresh to render the logout button
+          navigate('/'); // Redirect to dashboard page
+          
+        } else {
+          // Failed login logic here
+          console.log(data.error);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
+
+
   return (
     <div className='mt-[50px] ml-6'>
         <div className='text-[50px]'>Login</div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <div className='mt-2'>
             <label>Username:</label>
-            <input type="text"  className='border-[1px] border-solid border-gray-300 rounded-lg ml-4 mb-1' />
+            <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className='border-[1px] border-solid border-gray-300 rounded-lg ml-4 mb-1' />
             </div>
             <div>
             <label>Password:</label>
-            <input type="password"  className='border-[1px] border-solid border-gray-300 rounded-lg ml-4 mb-1' />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className='border-[1px] border-solid border-gray-300 rounded-lg ml-4 mb-1' />
             </div>
             <button type="submit" className='border-[1px] border-solid rounded-full bg-[#87CEEB] px-5 py-1 text-white text-lg mt-2'>Login</button>
         </form>
