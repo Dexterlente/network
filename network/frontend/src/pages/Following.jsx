06d41cd0f9bd92a52dd0e3f7e06e046977955e52
Dexterlente <dexter.lente18@gmail.com>
2023-04-18 01:24:34 +0800

    Pushed align

--- network/frontend/src/pages/Following.jsx
+++ network/frontend/src/pages/Following.jsx
@@ -42,11 +42,11 @@ const Following = () => {
         </div>
       <ul>
         {following.map(follower => (
-          <div key={follower.pk} className='grid grid-cols-10 py-4  hover:bg-gray-200 transition duration-300 ease-in-out'>
-            <div className='ml-2'>
+          <div key={follower.pk} className='flex py-4  hover:bg-gray-200 transition duration-300 ease-in-out'>
+            <div className='ml-4'>
              <img src={follower.image} className='h-16 w-16 rounded-full' />
             </div>
-            <div className='col-span-9 ml-2 mt-1'>
+            <div className='ml-2 mt-1'>
                   <div className='font-bold'>{follower.first_name} {follower.last_name}</div>
                   <div>@{follower.profile_username}</div>
             </div>
