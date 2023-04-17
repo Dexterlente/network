72a6303ef38eb334510ecf49291da0a1314625d5
Dexterlente <dexter.lente18@gmail.com>
2023-04-17 20:24:31 +0800

    Fixed

--- network/frontend/src/pages/MainBody.jsx
+++ network/frontend/src/pages/MainBody.jsx
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import pretty from '../assets/pretty.png';
 import { AiOutlineHeart } from 'react-icons/ai';
 import API_ENDPOINT from '../config.jsx' 
+import { Link } from 'react-router-dom';
 
 const MainBody = () => {
+  // const { id } = useParams();
   const [postBody, setPostBody] = useState([]);
 
   useEffect(() => {
@@ -24,7 +26,7 @@ const MainBody = () => {
                   <img src={pretty} alt='DefaultPic' className='h-[70px] w-[70px] rounded-full mt-8 ml-3' />
                   }
               </div>          
-            <div className='ml-5 font-bold mt-2 hover:underline cursor-pointer'> {post.poster_first_name} {post.poster_last_name}</div>
+            <Link to={`/profile/${post.poster_id}`} className='ml-5 font-bold mt-2 hover:underline cursor-pointer'> {post.poster_first_name} {post.poster_last_name}</Link>
             <div className='ml-2 mt-2'> @{post.poster_username}</div>
             <div className='ml-2 mt-2'>      
             {new Date(post.created_date).toLocaleDateString("en-US", {
